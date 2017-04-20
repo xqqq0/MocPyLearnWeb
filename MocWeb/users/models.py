f7e1302140d67209c30b550bd473d3ee692af9fa
程序小白猿 <15301072780@163.com>
2017-04-20 15:28:58 +0800

    user表创建字段

---
+++ MocWeb/users/models.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    gender = models.CharField(choices=(("male", u"男"), ("female", u"女")), default="female")
+    address = models.CharField(max_length=100, verbose_name=u"地址", default=u"")
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to="image/%Y/%M", default=u"image/default.png", max_length=100)
